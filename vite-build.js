fe9710bf7ba5e67e5cb81e401f6543581194b092
Khaled Elmorsy <khaled.mg.a@gmail.com>
2023-05-09 12:18:11 +0300

    Update Vite config with production mode

--- vite-build.js
+++ vite-build.js
@@ -3,14 +3,17 @@ const { build, mergeConfig } = require('vite');
 const fs = require('fs/promises');
 const path = require('path');
 const cssInjectedByJsPlugin = require('vite-plugin-css-injected-by-js').default;
-const rollupChromeReload = require('./ChromeReloadPlugin').rollupChromeReload
+const rollupChromeReload = require('./ChromeReloadPlugin').rollupChromeReload;
 
 /** @typedef {import('vite').UserConfig} ViteConfig */
 
 const dir = process.cwd();
 const args = process.argv.slice(2);
 
-const watch = !!args.find((arg) => ['-w', '--watch'].includes(arg));
+const { watch, production } = Object.entries({
+  watch: ['-w', '--watch'],
+  production: ['-p', '--production'],
+}).reduce((o, [f, t]) => ({ ...o, [f]: args.some((a) => t.includes(a)) }), {});
 
 function getConfig(entry, entryPath, configOverride) {
   /** @type {ViteConfig} */
@@ -31,32 +34,44 @@ function getConfig(entry, entryPath, configOverride) {
       outDir: '../dist',
       emptyOutDir: false,
       watch: watch ? {} : null,
+      sourcemap: !production,
     },
   };
 
   return configOverride ? mergeConfig(baseConfig, configOverride) : baseConfig;
 }
 
-const contentScriptConfig = {
-  plugins: [cssInjectedByJsPlugin()],
-};
-
 const copyStaticFiles = async () =>
   fs.cp(path.resolve(dir, './static'), path.resolve(dir, './dist'), {
     recursive: true,
   });
 
-const backgroundConfig = {
-  plugins: [rollupChromeReload()]
+/** @type {Object<string, ViteConfig>} */
+const configs = {
+  contentScript: {
+    plugins: [cssInjectedByJsPlugin()],
+  },
+  background: {
+    plugins: [...(production ? [] : [rollupChromeReload()])],
+  },
+  popup: {
+    build: {
+      rollupOptions: {
+        output: {
+          assetFileNames: 'popup.[ext]',
+        },
+      },
+    },
+  },
 };
-
 
 (async () => {
   await fs.rm(path.resolve(dir + '/dist'), { recursive: true }).catch(() => {});
 
-  build(getConfig('content-script', 'content-script.js', contentScriptConfig));
-  build(getConfig('background', 'background.js', backgroundConfig));
-  build(getConfig('popup', 'popup.js'));
+  build(
+    getConfig('content-script', 'content-script.js', configs.contentScript)
+  );
+  build(getConfig('background', 'background.js', configs.background));
+  build(getConfig('popup', 'popup.js', configs.popup));
   copyStaticFiles();
-
 })();
